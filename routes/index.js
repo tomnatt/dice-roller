68a570d770eb13c07c156b2fe6597b434b87a092
tomnatt <github@tomnatt.com>
2015-03-12 13:53:56 +0000

    starting to dissect user parameter

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,47 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var x = req.query.roll;
+
+  // var parts = splitRoll(req.query.roll);
+
+  rollDice(req.query.roll);
+
+  var x = 6;
   var number = rollDX(x)
 
   var output = {};
   output.number = number;
+
+  // output.parts = parts;
   res.json(output);
 });
 
 module.exports = router;
 
+// rolls a specific dX
 function rollDX(max) {
   var min = 1;
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// rolls a collection of dice (eg 2d6)
+function rollDice(roll) {
+  var bits = roll.split("d");
+
+  var number = bits[0];
+  var dice = bits[1];
+
+  number = (number == "" ? 1 : number);
+  console.log("Number: " + number);
+  console.log("Type: " + dice);
+}
+
+function splitRoll(roll) {
+
+  // split on +|-
+  var compenents = roll.split(/\+|\-/);
+  return compenents;
+
 }
 
 // 3. multiple dice rolling from parameter
