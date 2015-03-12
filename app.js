0a56d7af8adaf7def77ee486be409319bb70f114
tomnatt <github@tomnatt.com>
2015-03-12 13:26:44 +0000

    changed urls

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var rolls = require('./routes/rolls.js');
-
+var rolls = require('./routes/rolls');
 var app = express();
 
 // view engine setup
@@ -20,8 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/roll', rolls);
+app.use('/', rolls);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
