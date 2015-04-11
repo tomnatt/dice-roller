2135c53f4ed3916021f2ee828edbf0fd3ec276be
tomnatt <github@tomnatt.com>
2015-04-11 19:54:13 +0100

    check setting of roller correctly and stub roller

--- test/test_luck.js
+++ test/test_luck.js
@@ -1,6 +1,7 @@
 var expect = require('chai').expect,
     Luck = require('../lib/luck'),
     BasicLuckParser = require('../lib/luck_parsers/basic_luck_parser'),
+    d20Roller = require('../lib/luck_rollers/d20_roller'),
     Dice = require('../lib/dice');
 
 describe('test the luck object', function() {
@@ -14,8 +15,13 @@ describe('test the luck object', function() {
   });
 
   it('defaults the luck parser correctly', function() {
+    // default when not specified
     var luck1 = new Luck('2d6+1');
     expect(luck1.luck_parser).to.be.an.instanceOf(BasicLuckParser);
+
+    // default when null passed
+    var luck2 = new Luck('2d6+1', null);
+    expect(luck2.luck_parser).to.be.an.instanceOf(BasicLuckParser);
   });
 
   it('sets the luck parser correctly', function() {
@@ -23,13 +29,21 @@ describe('test the luck object', function() {
     expect(luck1.luck_parser).to.be.an.instanceOf(BasicLuckParser);
   });
 
-  // it('defaults the luck parser correctly', function() {
+  it('defaults the luck parser correctly', function() {
+    // default when not specified
+    var luck1 = new Luck('2d6+1');
+    expect(luck1.luck_roller).to.be.an.instanceOf(d20Roller);
 
-  // });
+    // default when null passed
+    var luck2 = new Luck('2d6+1', null, null);
+    expect(luck2.luck_parser).to.be.an.instanceOf(BasicLuckParser);
+    expect(luck2.luck_roller).to.be.an.instanceOf(d20Roller);
+  });
 
-  // it('sets the luck roller correctly', function() {
-
-  // });
+  it('sets the luck roller correctly', function() {
+    var luck1 = new Luck('2d6+1', null, new d20Roller());
+    expect(luck1.luck_roller).to.be.an.instanceOf(d20Roller);
+  });
 
   // it('rolls correctly', function() {
 
