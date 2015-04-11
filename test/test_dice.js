9319a2bfe3dfb40f4a0c7957eb3b02b970dffd5e
tomnatt <github@tomnatt.com>
2015-04-11 18:26:16 +0100

    added rolling test and simplified rolling code

--- test/test_dice.js
+++ test/test_dice.js
@@ -13,4 +13,21 @@ describe('test the dice object', function() {
     expect(dice.sides).to.equal(8);
   });
 
+  it('rolls correctly', function() {
+    var d6 = new Dice();
+    expect(d6.roll()).to.be.within(1,6);
+    expect(d6.roll()).to.be.within(1,6);
+    expect(d6.roll()).to.be.within(1,6);
+
+    var d10 = new Dice(10);
+    expect(d10.roll()).to.be.within(1,10);
+    expect(d10.roll()).to.be.within(1,10);
+    expect(d10.roll()).to.be.within(1,10);
+
+    var d4 = new Dice(4);
+    expect(d4.roll()).to.be.within(1,4);
+    expect(d4.roll()).to.be.within(1,4);
+    expect(d4.roll()).to.be.within(1,4);
+  })
+
 });
