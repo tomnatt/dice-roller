bdceee11ec6e977785f9c4ed56617c0ad7152926
tomnatt <github@tomnatt.com>
2015-04-11 19:29:28 +0100

    inject the BasicLuckParser

--- test/test_luck.js
+++ test/test_luck.js
@@ -1,5 +1,6 @@
 var expect = require('chai').expect,
     Luck = require('../lib/luck'),
+    BasicLuckParser = require('../lib/luck_parsers/basic_luck_parser'),
     Dice = require('../lib/dice');
 
 describe('test the luck object', function() {
@@ -12,9 +13,10 @@ describe('test the luck object', function() {
     expect(luck2.luck_string).to.equal('3d10+5');
   });
 
-  // it('sets the luck parser correctly', function() {
-
-  // });
+  it('sets the luck parser correctly', function() {
+    var luck1 = new Luck('2d6+1');
+    expect(luck1.luck_parser).to.be.an.instanceOf(BasicLuckParser);
+  });
 
   // it('sets the luck roller correctly', function() {
 
@@ -24,9 +26,10 @@ describe('test the luck object', function() {
 
   // });
 
-  // it('defaults the luck parser correctly', function() {
-
-  // });
+  it('defaults the luck parser correctly', function() {
+    var luck1 = new Luck('2d6+1');
+    expect(luck1.luck_parser).to.be.an.instanceOf(BasicLuckParser);
+  });
 
   // it('rolls correctly', function() {
 
